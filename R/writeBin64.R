9ca6b151cfbd37a1bc49f0e80af2f54779151008
Angus H. Wright <awright@astro.rub.de>
2022-03-25 09:28:02 +0100

    updated writeBin64 for R>=4.*

--- R/writeBin64.R
+++ R/writeBin64.R
@@ -11,11 +11,11 @@ writeBin64 = function(object, con, size = NA_integer_, endian = .Platform$endian
         chunks = ceiling(length(object)*size/(2^30))
         excess = (length(object) %% (2^30))
         for(i in 1:(chunks-1)){
-            writeBin(object[1:((2^30)/size)+(i-1)*((2^30)/size)], con, size, swap, useBytes)
+            writeBin(object=object[1:((2^30)/size)+(i-1)*((2^30)/size)], con=con, size=size, endian=endian, useBytes=useBytes)
         }
-        writeBin(object[1:excess+(chunks-1)*((2^30)/size)], con, size, swap, useBytes)
+        writeBin(object=object[1:excess+(chunks-1)*((2^30)/size)], con=con, size=size, endian=endian, useBytes=useBytes)
     }else{
-        writeBin(object, con, size, swap, useBytes)
+        writeBin(object=object, con=con, size=size, endian=endian, useBytes=useBytes)
     }
 }
 
